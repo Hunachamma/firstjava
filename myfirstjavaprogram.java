6da3f30704c23ea3b0b32b68c33286e32770550e
U-intel-PC\intel <leenap162@gmail.com>
2020-05-15 17:16:12 +0530

    leenafirst

--- myfirstjavaprogram.java
+++ myfirstjavaprogram.java
@@ -5,6 +5,9 @@ public class MyFirstJavaProgram {
     */
 
    public static void main(String []args) {
-      System.out.println("Hello World"); // prints Hello World
+      System.out.println("Hello World"); 
+// prints Hello World
+   System.out.println("LeenaRocks");
    }
+
 }
